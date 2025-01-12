9182b844c4466dca6ec9fabff05ff2b06705d4ba
AbasseMahmoud <barry96mamoudou@gmail.com>
2025-01-12 03:07:21 +0100

    Modifier

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const port = 8000
 const app = express()
 
+// Lancement sur la navigateur
+app.get('/',(req,res) =>{
+    res.send('Bienvue')
+})
 app.listen(port,()=>{
-    console.log(`http://localhost:${port}`)
+    console.log(`Démarrage au port http://localhost:${port}`)
 })

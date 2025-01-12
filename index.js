2bed167f3b3c1607a3dca0f88d42e7f422eebb36
AbasseMahmoud <barry96mamoudou@gmail.com>
2025-01-12 12:26:33 +0100

    Ajout etudiant

--- index.js
+++ index.js
@@ -3,10 +3,34 @@ const express = require('express')
 const port = 8000
 const app = express()
 
+students = [
+    {
+        "nom":"Barry",
+        "prenom":"Mahmoud",
+        "Adresse":"Tanaff",
+        "Telephone":78453412,
+    },
+    {
+        "nom":"Barry",
+        "prenom":"Mahmoud",
+        "Adresse":"Tanaff",
+        "Telephone":78453412,
+    },
+    {
+        "nom":"Barry",
+        "prenom":"Mahmoud",
+        "Adresse":"Tanaff",
+        "Telephone":78453412,
+    },
+]
+app.get('/students',(req,res) =>{
+    res.status(200).json(students);
+})
 // Lancement sur la navigateur
 app.get('/',(req,res) =>{
     res.send('Bienvue')
-})
+});
+
 app.listen(port,()=>{
     console.log(`Démarrage au port http://localhost:${port}`)
 })
